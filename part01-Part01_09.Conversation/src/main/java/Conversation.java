795ccd684a08fcf72979d7cc913f7cbadaf68ce7
Ada Norman <88652665+Adajen@users.noreply.github.com>
2023-01-12 21:50:55 +0600

    Add files via upload

---
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Greetings! How are you doing?");
+        String first = reader.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String second = reader.nextLine();
+        System.out.println("Thanks for sharing!");
+        
+
+    }
+
+}
