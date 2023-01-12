bf5fd1fc3cdefdc7fcd1cf54d100f22e52677f9b
Ada Norman <88652665+Adajen@users.noreply.github.com>
2023-01-12 21:52:49 +0600

    Add files via upload

---
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Ancient {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give a year:");
+        int first = Integer.valueOf(scan.nextLine());
+
+        int answer = first;
+        if (answer < 2015) {
+            System.out.println("Ancient history!");
+        }
+
+    }
+}
