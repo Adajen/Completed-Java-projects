a92507d39175c95033c2640a7c0927d0b9205ff0
Ada Norman <88652665+Adajen@users.noreply.github.com>
2023-01-12 21:51:00 +0600

    Add files via upload

---
+++ part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class AdditionFormula {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        
+         System.out.println("Give the second number:");
+        int second = Intrintlneger.valueOf(scanner.nextLine());
+        System.out.println( first + " + " + second + " = " + (first + second));
+        
+        
+        
+        
+        
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
